e114b4b26e330f01c3e0c12671b93e6968bde742
seonwoo <49448330+melly8954@users.noreply.github.com>
2026-06-01 19:34:32 +0900

    chore: 프로젝트 init

---
+++ src/main/java/com/linker/relia/ReliaApplication.java
@@ -0,0 +1,13 @@
+package com.linker.relia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReliaApplication.class, args);
+    }
+
+}

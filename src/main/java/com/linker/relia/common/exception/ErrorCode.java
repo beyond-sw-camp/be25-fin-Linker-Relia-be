129dd1892a4428db6b32b6f0c8a9df3c93259cad
seonwoo <49448330+melly8954@users.noreply.github.com>
2026-06-02 15:21:24 +0900

    feat: JWT 토큰 관리 예외 코드 추가

--- src/main/java/com/linker/relia/common/exception/ErrorCode.java
+++ src/main/java/com/linker/relia/common/exception/ErrorCode.java
@@ -9,6 +9,16 @@ public enum ErrorCode {
     USER_RESIGNED("USER_AUTH_004", "퇴직 또는 해촉된 사용자입니다.", HttpStatus.FORBIDDEN),
     INVALID_LOGIN_REQUEST("USER_AUTH_005", "로그인 요청 파싱 실패", HttpStatus.UNAUTHORIZED),
 
+    ACCESS_TOKEN_NOT_FOUND("auth_access_001", "Access Token을 요청받지 못했습니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_EXPIRED("auth_access_002", "만료된 Access Token입니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_INVALID("auth_access_003", "유효하지 않은 Access Token입니다.", HttpStatus.UNAUTHORIZED),
+    ACCESS_TOKEN_OF_BLACK_LIST("auth_access_004", "해당 Access Token은 BlackList 토큰입니다.", HttpStatus.CONFLICT),
+
+    REFRESH_TOKEN_NOT_FOUND("auth_refresh_001", "Refresh Token을 요청받지 못했습니다.", HttpStatus.UNAUTHORIZED),
+    REFRESH_TOKEN_EXPIRED("auth_refresh_002", "만료된 Refresh Token입니다.", HttpStatus.UNAUTHORIZED),
+    REFRESH_TOKEN_INVALID("auth_refresh_003", "유효하지 않은 Refresh Token입니다.", HttpStatus.UNAUTHORIZED),
+    REFRESH_TOKEN_NOT_IN_REDIS("auth_refresh_004", "Refresh Token이 Redis에 존재하지 않습니다.", HttpStatus.UNAUTHORIZED),
+
     INVALID_REQUEST("COMMON_001", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
     INTERNAL_SERVER_ERROR("COMMON_002", "서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 

cbe0076951fec6ea8b17f73f9903ce0468403966
haily <49448330+melly8954@users.noreply.github.com>
2026-06-06 17:45:43 +0900

    feat: 계약, 상담 엔티티 매핑을 위한 임시 도메인

---
+++ src/main/java/com/linker/relia/contract/domain/Contract.java
@@ -0,0 +1,44 @@
+package com.linker.relia.contract.domain;
+
+import com.linker.relia.customer.domain.Customer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "contracts")
+public class Contract {
+    @Id
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(name = "id")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Column(name = "monthly_premium")
+    private BigDecimal monthlyPremium;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}

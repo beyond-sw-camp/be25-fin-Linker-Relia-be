169ae2a529f773a67f905b15f2e82afbef1f846f
yeaxyzi <yeaxyzi@gmail.com>
2026-06-10 14:13:01 +0900

    feat: 보종 엔티티 추가

---
+++ src/main/java/com/linker/relia/insurance/domain/InsuranceCategory.java
@@ -0,0 +1,39 @@
+package com.linker.relia.insurance.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "insurance_categories")
+public class InsuranceCategory {
+    @Id
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "insurance_category_code")
+    private String insuranceCategoryCode;
+
+    @Column(name = "insurance_category_name")
+    private String insuranceCategoryName;
+
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
+
